bb878a0ed92f772f7df715fbdee4e675deb4d135
David <davbetm@gmail.com>
2019-07-10 23:30:38 -0500

    Intro a Dart-lang, primeros ejemplos

---
+++ intro-dart/1_holaMundo.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Hola mundo");
+  var nombre = "David";
+  String otroNombre = "David2";
+  print("Hola " + nombre);
+  print("Hola $nombre");
+}
---
+++ intro-dart/2_NumerosYStrings.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Números
+  int empleados = 45;
+  double pi = 3.14159;
+  double numero = 1.0;
+  print("$empleados - $pi - $numero");
+  
+  // Cadenas
+  String nombre = "Dav\'id";
+  print(nombre[0]);
+  print(nombre);
+  print(nombre[nombre.length-1]);
+}
